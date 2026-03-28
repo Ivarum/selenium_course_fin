87a22ffebd92189abdc5477d1c2bd3198d7c3830
Иван Румов <rymovjk@gmail.com>
2026-03-28 18:34:05 +0300

    login use in test body

--- test_main_page.py
+++ test_main_page.py
@@ -4,7 +4,9 @@ import time
 
 
 def test_guest_can_go_to_login_page(browser):
-     link = "https://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer"
-     page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
-     page.open()  # открываем страницу
-     page.should_be_login_link()          # выполняем метод страницы — переходим на страницу логина
+    link = "http://selenium1py.pythonanywhere.com"
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()         
