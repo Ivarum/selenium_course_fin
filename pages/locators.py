b5c52476754980f15085b7b820109452aed57982
Иван Румов <rymovjk@gmail.com>
2026-03-28 20:09:27 +0300

    product page testing

--- pages/locators.py
+++ pages/locators.py
@@ -3,5 +3,14 @@ from selenium.webdriver.common.by import By
 class MainPageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
 class LoginPageLocators():
-    LOG_FORM = ((By.CSS_SELECTOR, "#login_form"))
-    REG_FORM = ((By.CSS_SELECTOR, "#register_form"))
+    LOG_FORM = (By.CSS_SELECTOR, "#login_form")
+    REG_FORM = (By.CSS_SELECTOR, "#register_form")
+class ProductPageLocators():
+    ADD_BTN = (By.CSS_SELECTOR, ".btn-add-to-basket")
+    
+    SUCCES_ADD_MSG = (By.CSS_SELECTOR, ".alert-success:first-of-type .alertinner strong")
+    NAME = (By.CSS_SELECTOR, ".product_main h1")
+    
+    BASKET_VAL = (By.CSS_SELECTOR, ".alertinner p strong")
+    COST = (By.CSS_SELECTOR, ".product_main .price_color")
+    
---
+++ pages/product_page.py
@@ -0,0 +1,11 @@
+from .base_page import BasePage
+from .locators import ProductPageLocators
+
+class ProductPage(BasePage):
+    def add_product_to_backet(self):
+        add_btn = self.browser.find_element(*ProductPageLocators.ADD_BTN)
+        add_btn.click()
+    def add_to_basket_name_check(self):
+        assert self.browser.find_element(*ProductPageLocators.SUCCES_ADD_MSG).text == self.browser.find_element(*ProductPageLocators.NAME).text, "Not correct product name in message"
+    def add_to_basket_cost_check(self): 
+        assert self.browser.find_element(*ProductPageLocators.BASKET_VAL).text == self.browser.find_element(*ProductPageLocators.COST).text, "Not correct product cost in message"
