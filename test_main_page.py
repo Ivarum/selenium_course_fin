2325ceb404134beb69109560ab2491a402f9d92b
Иван Румов <rymovjk@gmail.com>
2026-03-28 18:25:34 +0300

    login link methods

--- test_main_page.py
+++ test_main_page.py
@@ -1,9 +1,10 @@
 from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
 import time
 
 
 def test_guest_can_go_to_login_page(browser):
-    link = "https://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer"
-    page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
-    page.open()  # открываем страницу
-    page.should_be_login_link()          # выполняем метод страницы — переходим на страницу логина
+     link = "https://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer"
+     page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
+     page.open()  # открываем страницу
+     page.should_be_login_link()          # выполняем метод страницы — переходим на страницу логина
